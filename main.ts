f6d718dd278224046fd0968e647df06667d16ee3
nonamewyc <110407680+nonamewyc@users.noreply.github.com>
2022-08-01 23:43:17 +0800

    to be rewritten

--- main.ts
+++ main.ts
@@ -1,3 +1,46 @@
+function lf () {
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 30)
+    } else {
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 30)
+            if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 30)
+            }
+        } else {
+            if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 30)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+                if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+                    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 30)
+                    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+                }
+                if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+                    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 30)
+                } else {
+                    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+                }
+            }
+        }
+    }
+}
 basic.forever(function () {
-	
+    lf()
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) <= 5) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 100)
+        basic.pause(100)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 30)
+        basic.pause(500)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 100)
+        basic.pause(100)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 30)
+        basic.pause(100)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
+    }
 })
